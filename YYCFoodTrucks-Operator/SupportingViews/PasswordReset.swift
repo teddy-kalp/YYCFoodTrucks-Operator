3293de7ebd7d416438b45bad0dc0d0f36d5f5fc0
admin <admin@admins-MacBook-Air.local>
2020-11-24 14:17:49 -0700

    password reset function added

---
+++ YYCFoodTrucks-Operator/SupportingViews/PasswordReset.swift
@@ -0,0 +1,73 @@
+//
+//  PasswordReset.swift
+//  YYCFoodTrucks-Operator
+//
+//  Created by admin on 2020-11-22.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+
+struct PasswordReset: View {
+    
+    //var auth = Firebase.auth();
+    
+
+    @EnvironmentObject var session: SessionStore
+    @Environment(\.colorScheme) var colorScheme
+    @State var user_email: String = ""
+    @State var errorText: String = ""
+    
+    var body: some View {
+        NavigationView(){
+            VStack(alignment: .center){
+                Text("Enter your account email address")
+                    //.font(.callout)
+                    .bold()
+                    .font(.system(size:18))
+                
+                TextField("Operator email address", text: $user_email)
+                    .font(Font.system(size: 25, design: .default))
+                    .frame(width: 300, height: 70)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button(action: {
+                    Auth.auth().sendPasswordReset(withEmail: user_email, completion: ({ error in
+                        if let error = error {
+                            self.errorText = error.localizedDescription
+                        return
+                    }
+                }))
+                    
+                }) {
+                    Text("RESET")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(primColor)
+                        .shadow(radius: 10)
+                        .cornerRadius(15.0)
+                        
+                }
+                Spacer()
+            } .padding(.top, 50)
+                .frame(width: 300)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            .navigationBarTitle("Request Password Reset", displayMode: .inline)
+            
+                    
+        }
+    }
+
+}
+    
+    
+    
+    
+
+
+
+
